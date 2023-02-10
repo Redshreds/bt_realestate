67ef6a8405e98caab4acca77b1e95ecb21bcdeb0
Brandon Bowen <aghoraphobic@gmail.com>
2023-02-10 01:20:29 -0800

    Added static links to Jquery, Bootstrap, CSS elements

--- bt_realestate/settings.py
+++ bt_realestate/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os # CHECK BACK HERE -- OFF SCRIPT!
+import os # CHECK BACK HERE -- OFF SCRIPT! !!! I'M A GENIUS !!!
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'bt_realestate/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
