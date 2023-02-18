6c11c30506d4b7472117d9f938839bab344d580d
Brandon Bowen <aghoraphobic@gmail.com>
2023-02-17 21:27:15 -0800

    1.02

--- .history/bt_realestate/pages/apps_20230209222641.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PagesConfig(AppConfig): # will go into settings file
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pages'
--- .history/bt_realestate/realtors/admin_20230211231147.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Realtor
-
-admin.site.register(Realtor)
-# Register your models here.
