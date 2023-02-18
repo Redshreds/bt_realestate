600292758e93d48a172af0ab4a0c79fcacf1a0e6
Brandon Bowen <aghoraphobic@gmail.com>
2023-02-18 03:23:30 -0800

    Removed email from settings

--- bt_realestate/settings.py
+++ bt_realestate/settings.py
@@ -150,13 +150,6 @@ try:
 except ImportError:
     pass
 
-#Email Config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT =    587
-EMAIL_HOST_USER=''
-EMAIL_HOST_PASSWORD=''
-EMAIL_USE_TLS=True
-
 try:
     from .local_settings import *
 except ImportError:
